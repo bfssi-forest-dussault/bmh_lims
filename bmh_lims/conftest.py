639443ab2660e3ec28f60f9557ae17aeee687dde
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-09-17 11:41:28 -0400

    Initial commit

---
+++ bmh_lims/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bmh_lims.users.models import User
+from bmh_lims.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
---
+++ bmh_lims/users/tests/test_models.py
@@ -0,0 +1,9 @@
+import pytest
+
+from bmh_lims.users.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
