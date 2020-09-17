76b4f83905cf749fba62eb0329df12ebe81780f4
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-09-17 16:18:27 -0400

    register verbose name for FrontendConfig

--- bmh_lims/frontend/apps.py
+++ bmh_lims/frontend/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class FrontendConfig(AppConfig):
     name = 'bmh_lims.frontend'
+    verbose_name = _("Frontend")
