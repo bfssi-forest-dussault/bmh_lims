4930fb1fa7f0911e74b0361f4ad2362c87975707
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-09-17 16:16:20 -0400

    register sample route

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from bmh_lims.users.api.views import UserViewSet
+from bmh_lims.database.api.views import SampleViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -9,7 +10,7 @@ else:
     router = SimpleRouter()
 
 router.register("users", UserViewSet)
-
+router.register("samples", SampleViewSet, basename='samples')
 
 app_name = "api"
 urlpatterns = router.urls
