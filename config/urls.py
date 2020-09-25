bf8fda3ed31740c04e529be05393f545ac5ff5ce
Rose Li <li.rose11719@gmail.com>
2020-09-25 11:04:50 -0400

    Change URL to accommodate any frontend URL

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
@@ -14,8 +15,7 @@ urlpatterns = [
                   path("users/", include("bmh_lims.users.urls", namespace="users")),
                   path("accounts/", include("allauth.urls")),
                   # Your stuff: custom urls includes go here
-                  path("lims/", include("bmh_lims.frontend.urls", namespace="frontend")),
-
+                  url(r'^(?:[a-z|A-Z]*)/?', include("bmh_lims.frontend.urls", namespace="frontend")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
