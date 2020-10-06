8eb2c2c1e2b66fa2741098890abb9ee4f15dfe53
Rose Li <li.rose11719@gmail.com>
2020-10-06 16:10:10 -0400

    Add parameterized testing

--- bmh_lims/frontend/spec/csvReader.test.js
+++ bmh_lims/frontend/spec/csvReader.test.js
@@ -1,6 +1,7 @@
 import { csvToJSON } from 'utils'
+import each from 'jest-each'
 
-const isNestedArray = (array1, array2) => {
+const isEqual = (array1, array2) => {
     let isEqual = true
     array1.forEach((row, idx) => {
         row.forEach((item, itemIdx) => {
@@ -9,9 +10,23 @@ const isNestedArray = (array1, array2) => {
             }
         })
     })
+    if (!isEqual) {
+        console.log(array1, array2)
+    }
     return isEqual
 }
 
+// test for simple Jest testing framework
 test('csvToJSON properly handles regular commas', () => {
-    expect(isNestedArray(csvToJSON('a, b, c'), [['a', 'b', 'c']])).toBe(true)
+    expect(isEqual(csvToJSON('a, b, c'), [['a', 'b', 'c']])).toBe(true)
+})
+
+// test for Jest-each's parameterized tests
+each`
+    input                  | output
+    ${'a, b, c'}           | ${[['a', 'b', 'c']]}
+    ${'a, b, c\nd, e, f'}  | ${[['a', 'b', 'c'], ['d', 'e', 'f']]}
+    ${'a, b, "c ""d"" e"'} | ${[['a', 'b', 'c "d" e']]}
+`.test('expect $input to match $output', ({input, output}) => {
+    expect(isEqual(csvToJSON(input), output)).toBe(true)
 })
