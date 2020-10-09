0a349bd27398d1cbd3747d631daf149f71aa6171
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-10-09 13:35:36 -0400

    Display history in admin panel for all models

--- bmh_lims/database/admin.py
+++ bmh_lims/database/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from bmh_lims.database import models
+from simple_history.admin import SimpleHistoryAdmin
 
 # Register your models here.
-admin.site.register(models.Sample)
-admin.site.register(models.WorkflowDefinition)
-admin.site.register(models.WorkflowBatch)
-admin.site.register(models.Project)
-admin.site.register(models.Lab)
-admin.site.register(models.DNAExtractionResults)
+admin.site.register(models.Sample, SimpleHistoryAdmin)
+admin.site.register(models.WorkflowDefinition, SimpleHistoryAdmin)
+admin.site.register(models.WorkflowBatch, SimpleHistoryAdmin)
+admin.site.register(models.Project, SimpleHistoryAdmin)
+admin.site.register(models.Lab, SimpleHistoryAdmin)
+admin.site.register(models.DNAExtractionResults, SimpleHistoryAdmin)
