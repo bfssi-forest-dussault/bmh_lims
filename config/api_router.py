91652e1878afa9308172bb66ef8deabc8f320eb8
bfssi-forest-dussault <forest.dussault@canada.ca>
2020-11-16 14:34:06 -0500

    register workflow_samplebatch_create endpoint

--- config/api_router.py
+++ config/api_router.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from bmh_lims.users.api.views import UserViewSet
 from bmh_lims.database.api.views import SampleViewSet, WorkflowBatchViewSet, LabViewSet, ProjectViewSet, \
-    WorkflowSampleViewSet, WorkflowDefinitionViewSet
+    WorkflowSampleViewSet, WorkflowDefinitionViewSet, WorkflowSampleBatchCreateViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -14,6 +14,7 @@ router.register("users", UserViewSet)
 router.register("samples", SampleViewSet, basename='samples')
 router.register("workflow_samples", WorkflowSampleViewSet, basename='workflow_samples')
 router.register("workflow_batches", WorkflowBatchViewSet, basename='workflow_batches')
+router.register("workflow_samplebatch_create", WorkflowSampleBatchCreateViewSet, basename='workflow_samplebatch_create')
 router.register("workflow_definitions", WorkflowDefinitionViewSet, basename='workflow_definitions')
 router.register("labs", LabViewSet, basename='labs')
 router.register("projects", ProjectViewSet, basename='projects')
