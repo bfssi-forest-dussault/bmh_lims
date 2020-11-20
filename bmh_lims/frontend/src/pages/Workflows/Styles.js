53c1e0e32155539c2b2b350bed60d36b8876f3da
Rose Li <li.rose11719@gmail.com>
2020-11-20 02:50:15 -0500

    Clean code

--- bmh_lims/frontend/src/pages/Workflows/Styles.js
+++ bmh_lims/frontend/src/pages/Workflows/Styles.js
@@ -9,3 +9,48 @@ const loadingAnimation = keyframes`
 export const LoadingIconContainer = styled.div`
     animation: ${loadingAnimation} 2s infinite;
 `
+
+export const PageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    align-items: center;
+`
+
+export const HeaderBar = styled.div`
+    display: flex;
+    justify-content: right;
+    width: 100%;
+    background-color: ${props => props.theme.colour2};
+    border-bottom: 2px solid ${props => props.theme.colour2};
+    color: white;
+    align-items: center;
+    height: 7%;
+`
+
+export const BodyArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 9;
+    width: 50%;
+    padding: 2% 25%;
+`
+
+export const TableContainer = styled.div`
+    width: 100%;
+    height: 50%;
+    position: relative;
+    margin-bottom: 1%;
+`
+
+export const LoadingContainer = styled.div`
+    width: 100%;
+    height: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
--- bmh_lims/frontend/src/pages/Workflows/index.js
+++ bmh_lims/frontend/src/pages/Workflows/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import styled, { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles'
 import {
     CircularButtonBar,
@@ -11,55 +11,17 @@ import {
     Notice,
     Table
 } from 'components'
-import { LoadingIconContainer } from './Styles'
+import {
+    BodyArea,
+    HeaderBar,
+    LoadingContainer,
+    LoadingIconContainer,
+    PageContainer,
+    TableContainer
+} from './Styles'
 import { IconContext } from 'react-icons'
 import { CgSearchLoading } from 'react-icons/cg'
 import axios from 'axios'
-
-const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    align-items: center;
-`
-
-const HeaderBar = styled.div`
-    display: flex;
-    justify-content: right;
-    width: 100%;
-    background-color: ${props => props.theme.colour2};
-    border-bottom: 2px solid ${props => props.theme.colour2};
-    color: white;
-    align-items: center;
-    height: 7%;
-`
-
-const BodyArea = styled.div`
-    display: flex;
-    flex-direction: column;
-    flex-grow: 9;
-    width: 50%;
-    padding: 2% 25%;
-`
-
-const TableContainer = styled.div`
-    width: 100%;
-    height: 50%;
-    position: relative;
-    margin-bottom: 1%;
-`
-
-const LoadingContainer = styled.div`
-    width: 100%;
-    height: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
 
 const AssignSection = ({theme}) => {
     const [samples, setSamples] = useState({headers: [], content: []})
