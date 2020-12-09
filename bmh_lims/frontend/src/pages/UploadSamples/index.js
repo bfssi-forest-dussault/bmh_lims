7ac43a33de0f9171518913b40630661f637c4186
Rose Li <li.rose11719@gmail.com>
2020-12-09 15:37:15 -0500

    POST with lab ids instead of lab names

--- bmh_lims/frontend/src/pages/UploadSamples/index.js
+++ bmh_lims/frontend/src/pages/UploadSamples/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Table, CombinedLogo, FilledButton, InvertedLinkButton, FileInputButton, Notice } from 'components'
@@ -10,69 +10,88 @@ import { theme } from 'styles'
 axios.defaults.xsrfHeaderName = "X-CSRFToken"
 axios.defaults.withCredentials = true
 
-const uploadHandler = (event, updateIsUploaded, updateContent, updateIsInvalid) => {
-    event.preventDefault()
-    const submittedFile = event.target.files[0]
-    updateIsUploaded(true)
-    if(isCSV(submittedFile.name)) {
-        csvReader(submittedFile, (sampleData) => updateContent({headers: sampleData[0], content: sampleData.slice(1, sampleData.length)}))
-    } else if (isExcel(submittedFile.name)) {
-        xlsxReader(submittedFile, (sampleData) => {
-            updateContent({headers: sampleData[0], content: sampleData.slice(1, sampleData.length)})
-        })
-    } else {
-        console.log('updating for invalid file')
-        updateIsInvalid(true)
-    }
-}
-
-const formatSampleData = (content) => {
-    const sampleData = content.content.map(row => row.reduce((acc, item, idx) => {
-        if (!!content.headers[idx]) {
-            acc[content.headers[idx]] = item
-        } else {
-            return false // TODO: Toast
-        }
-        return acc
-    }, {}))
-    return sampleData
-}
-
-const onClickSubmit = (event,content, submittedFile, updateSubmitted) => {
-    event.preventDefault()
-    if(submittedFile){
-        const sampleData = formatSampleData(content)
-        if (validateData(sampleData)) {
-            axios({
-                method: 'POST',
-                headers: {
-                    'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
-                    'Content-type': 'application/json'
-                },
-                url: '/api/samples/',
-                data: JSON.stringify(tableToData(content)) // TODO: Placeholder
-            }).then((res) => {
-                console.log('success')
-                console.log(res.data)
-                updateSubmitted({isSubmitted: true, isError: false, errorInfo: ''})
-            }).catch(rej => {
-                console.log('failed')
-                updateSubmitted({isSubmitted: true, isError: true, errorInfo: dataToString(rej.response.data)})
-            })
-        } else {
-            console.log('data not valid') // TODO: toast (also show info)
-        }
-    } else {
-        console.log('no file submitted!!')
-    }
-}
-
 // page currently looks quite strange. This will be styled better later
 const UploadSamplesPage = () => {
     const [isUploaded, updateIsUploaded] = useState(false)
     const [submitted, updateSubmitted] = useState({isSubmitted: false, isError: false, errorInfo: ''})
     const [isInvalid, updateIsInvalid] = useState(false)
-    const [content, updateContent] = useState({headers: ['AAA', 'BBB', 'CCC', 'DDD', 'EEE'], content: [...Array(50).keys()].map((item, idx) => ['aaa', 'bbb', 'ccc', 'ddd', 'eee'])})
+    const [content, updateContent] = useState({headers: [' ', ' ', ' ', ' ', ' '], content: [...Array(50).keys()].map((item, idx) => [' ', ' ', ' ', ' ', ' '])})
+    const [labs, updateLabs] = useState(null)
+
+    useEffect(() => {
+        const initializeLabs = async () => {
+            const labsRes = (await axios.get('/api/labs/')).data
+            const labsList = labsRes.reduce((acc, currentLab) => {
+                acc[currentLab.lab_name] = currentLab.id
+                return acc
+            }, {})
+            updateLabs(labsList)
+        }
+        initializeLabs()
+    }, [])
+
+    const uploadHandler = (event, updateIsUploaded, updateContent, updateIsInvalid) => {
+        event.preventDefault()
+        const submittedFile = event.target.files[0]
+        updateIsUploaded(true)
+        if(isCSV(submittedFile.name)) {
+            csvReader(submittedFile, (sampleData) => updateContent({headers: sampleData[0], content: sampleData.slice(1, sampleData.length)}))
+        } else if (isExcel(submittedFile.name)) {
+            xlsxReader(submittedFile, (sampleData) => {
+                updateContent({headers: sampleData[0], content: sampleData.slice(1, sampleData.length)})
+            })
+        } else {
+            console.log('updating for invalid file')
+            updateIsInvalid(true)
+        }
+    }
+    
+    const formatSampleData = (content) => {
+        const sampleData = content.content.map(row => row.reduce((acc, item, idx) => {
+            if (!!content.headers[idx]) {
+                acc[content.headers[idx]] = item
+            } else {
+                return false // TODO: Toast
+            }
+            return acc
+        }, {}))
+        return sampleData
+    }
+    
+    const onClickSubmit = (event, content, submittedFile, updateSubmitted) => {
+        const tabularSamples = tableToData(content)
+        const sanitizedSamples = tabularSamples.map(({submitting_lab, ...otherFields}) => ({
+            submitting_lab: labs[submitting_lab],
+            ...otherFields
+        }))
+        event.preventDefault()
+        if(submittedFile){
+            const sampleData = formatSampleData(content)
+            if (validateData(sampleData)) {
+                axios({
+                    method: 'POST',
+                    headers: {
+                        'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
+                        'Content-type': 'application/json'
+                    },
+                    url: '/api/samples/',
+                    data: JSON.stringify(sanitizedSamples) // TODO: Placeholder
+                }).then((res) => {
+                    console.log('success')
+                    console.log(res.data)
+                    updateSubmitted({isSubmitted: true, isError: false, errorInfo: ''})
+                }).catch(rej => {
+                    console.log('failed')
+                    updateSubmitted({isSubmitted: true, isError: true, errorInfo: dataToString(rej.response.data)})
+                })
+            } else {
+                console.log('data not valid') // TODO: toast (also show info)
+            }
+        } else {
+            console.log('no file submitted!!')
+        }
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <PageContainer>
